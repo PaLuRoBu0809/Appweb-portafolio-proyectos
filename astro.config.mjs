bbd7dfdc00ccc2dca02e845380bb59ed15ab359a
PaLuRoBu0809 <pedroperezgay12244@gmail.com>
2026-06-26 13:45:46 -0500

    Cambiar adaptador de Node a Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel,
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
+  
   },
 });
